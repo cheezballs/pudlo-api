01ff90324d42866673e99f84b7a53dfc0293f173
Matt Roberts <sweetcrapmatt@gmail.com>
2023-02-20 11:52:31 -0600

    Trying more stuff.

---
+++ src/main/java/us/mattroberts/pudlo/PudloApplication.java
@@ -0,0 +1,14 @@
+package us.mattroberts.pudlo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PudloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PudloApplication.class, args);
+    }
+
+}
